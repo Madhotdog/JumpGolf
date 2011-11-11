578d7daaa8399d53c27a648d25c369821746be04
Joel Herber <joelherber@Joel-Herbers-MacBook-Pro.local>
2011-11-11 20:39:49 +0000

    Initial Commit

---
+++ JumpGolf/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  JumpGolf
+//
+//  Created by Joel Herber on 11/11/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ JumpGolf/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  JumpGolf
+//
+//  Created by Joel Herber on 11/11/2011.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
